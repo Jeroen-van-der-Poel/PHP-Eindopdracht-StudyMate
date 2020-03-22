d66cdec7b490bd5b423f4be3e1172ac45124e9c6
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-03-22 14:49:17 +0100

    tables

--- PHP_Eindopdracht/app/Http/Controllers/AdminController.php
+++ PHP_Eindopdracht/app/Http/Controllers/AdminController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Course;
 use App\Http\Controllers\Controller;
+use App\Teacher;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('Admin-Area/index');
+        $teachers = Teacher::orderBy('id', 'desc')->get();
+        $courses = Course::orderBy('id', 'desc')->get();
+        return view('Admin-Area/index' , compact('teachers','courses' ));
     }
 }
--- PHP_Eindopdracht/resources/views/Admin-Area/index.blade.php
+++ PHP_Eindopdracht/resources/views/Admin-Area/index.blade.php
@@ -1,6 +1,127 @@
 @extends('layouts.layout')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-lg-offset-1">
+                <div class="table-responsive">
+                    <div class="card-header">
+                        <i class="fas fa-chart-area"></i>
+                        Docenten overzicht
+                    </div>
+                    <table class="table table-bordered table-striped">
 
+                        <thead>
+                        <tr>
+                            <th>Naam</th>
+                            <th>Email</th>
+                            <th>Telefoonnummer</th>
+                            <th>Heeft les gegeven</th>
+                            <th>Operaties</th>
+
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach ($teachers as $key => $teacher)
+
+                            <tr>
+
+
+                                <td>{{ $teacher->name }}</td>
+                                <td>{{ $teacher->email }}</td>
+                                <td>{{ $teacher->phone_number }}</td>
+                                <td>{{ $teacher->has_taught }}</td>
+
+                                <td>
+
+                                    {{--                                <form action="/EditEvenement/{{$event->id}}" method="GET">
+                                                                        <div class="form-group">
+                                                                            <input type="submit" class="btn btn-success d-flex float-right" value="Wijzigen">
+                                                                        </div>
+                                                                    </form>
+
+                                                                    <form class="float-right" method="POST" action="/admin-area/events/{{$event->id}}">
+                                                                        {{ csrf_field() }}
+                                                                        {{ method_field('DELETE') }}
+
+                                                                        <div class="form-group">
+                                                                            <input type="submit" class="btn btn-danger d-flex float-right delete-event" value="Weigeren">
+                                                                        </div>
+                                                                    </form>--}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-lg-offset-1">
+                <div class="table-responsive">
+                    <div class="card-header">
+                        <i class="fas fa-chart-area"></i>
+                        Vakken overzicht
+                    </div>
+                    <table class="table table-bordered table-striped">
+
+                        <thead>
+                        <tr>
+                            <th>Naam</th>
+                            <th>Periode</th>
+                            {{--<th>Coördinator</th>--}}
+                            <th>Rol</th>
+                            <th>Operaties</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        @foreach ($courses as $course)
+
+
+                            <tr>
+
+                                <td>{{ $course->name }}</td>
+                                <td>{{ $course->period }}</td>
+                                <td>{{ $course->test_method}}</td>
+
+                                <td>
+                                    {{--                                    <form method="POST" action="/admin-area/users/accept-user/{{$user->id}}">
+                                                                            @csrf
+                                                                            {{ method_field('PUT') }}
+                                                                            <div class="form-group">
+                                                                                <select name="userRole" class="form-control">
+                                                                                    @foreach($roles as $role)
+                                                                                        @if($role->name == 'Company'|| $role->name=='Student' )
+                                                                                            <option value="{{$role->name}}">{{$role->name}}</option>
+                                                                                        @endif
+                                                                                    @endforeach
+                                                                                </select>
+                                                                            </div>
+                                                                            <div class="float-right">
+                                                                                <button type="submit" class="btn btn-success float-left">Accept</button>
+                                                                            </div>
+                                                                        </form>
+
+                                                                        <form class="float-right d-flex pr-2" method="POST" action="/admin-area/users/{{$user->id}}">
+                                                                            {{ csrf_field() }}
+                                                                            {{ method_field('DELETE') }}
+
+                                                                            <div class="form-group d-flex">
+                                                                                <input type="submit" class="btn btn-danger delete-user" value="Weigeren">
+                                                                            </div>
+                                                                        </form>--}}
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
