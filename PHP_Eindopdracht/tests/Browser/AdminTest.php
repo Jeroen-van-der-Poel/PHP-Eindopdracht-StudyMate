1d7fde59ca0cc68a03234a1ae18f92f8b14536d5
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-24 16:30:38 +0100

    fix

--- PHP_Eindopdracht/tests/Browser/AdminTest.php
+++ PHP_Eindopdracht/tests/Browser/AdminTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class DashboardTests extends DuskTestCase
+class AdminTest extends DuskTestCase
 {
     /**
      * A Dusk test example.
