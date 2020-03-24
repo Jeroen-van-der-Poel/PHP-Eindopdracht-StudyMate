9c469704101b6039cabdc25f26824534bcf9a400
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-24 16:05:25 +0100

    iets

--- PHP_Eindopdracht/tests/Browser/DeadlineTests.php
+++ PHP_Eindopdracht/tests/Browser/DeadlineTests.php
@@ -6,18 +6,18 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
+class DeadlineTests extends DuskTestCase
 {
     /**
-     * A basic browser test example.
-     *
+     * A Dusk test example.
+     * @test
      * @return void
      */
-    public function testBasicExample()
+    public function Can_Click_Add_Deadline()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/#')
-                    ->assertSee('Dashboard');
+            $browser->visit('/addDeadline')
+                    ->assertSee('Deadline aanmaken');
         });
     }
 }
