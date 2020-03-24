fa368b4e8303bf07681d935e4a01d1c8e1a1523d
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-24 16:27:57 +0100

    ellende

--- PHP_Eindopdracht/tests/Browser/DeadlineTests.php
+++ PHP_Eindopdracht/tests/Browser/DeadlineTests.php
@@ -14,11 +14,12 @@ class DeadlineTests extends DuskTestCase
      * @test
      * @return void
      */
-    public function Can_Click_Add_Deadline()
+
+    public function test_Can_Click_Add_Deadline()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/addDeadline')
-                    ->assertSee('Deadline aanmaken');
+                ->assertSee('Deadline');
         });
     }
 }
