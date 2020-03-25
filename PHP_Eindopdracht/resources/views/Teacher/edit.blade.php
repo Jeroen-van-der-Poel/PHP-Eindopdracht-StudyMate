f3307d8dd79a11e0f3ccdbe5491bcafb82ea339f
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-03-25 21:03:08 +0100

    teacher fix

--- PHP_Eindopdracht/resources/views/Teacher/edit.blade.php
+++ PHP_Eindopdracht/resources/views/Teacher/edit.blade.php
@@ -27,7 +27,7 @@
                     <div class="form-group row">
                         <label for="email" class="col-md-4 col-form-label pl-0">Email</label>
 
-                        <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $teacher->email }}" required autocomplete="email" autofocus>
+                        <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') ?? $teacher->email }}" autocomplete="email" autofocus>
 
                         @error('email')
                         <span class="invalid-feedback" role="alert">
@@ -39,7 +39,7 @@
                     <div class="form-group row">
                         <label for="phone_number" class="col-md-4 col-form-label pl-0">Telefoonnummer</label>
 
-                        <input id="phone_number" type="number" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') ?? $teacher->phone_number }}" required autocomplete="phone_number" autofocus>
+                        <input id="phone_number" type="number" class="form-control @error('phone_number') is-invalid @enderror" name="phone_number" value="{{ old('phone_number') ?? $teacher->phone_number }}" autocomplete="phone_number" autofocus>
 
                         @error('phone_number')
                         <span class="invalid-feedback" role="alert">
