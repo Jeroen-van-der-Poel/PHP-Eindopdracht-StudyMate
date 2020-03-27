624f4e0b047f8da12f4219448da1a5c2718df0f6
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-03-27 15:34:20 +0100

    sterkte

--- PHP_Eindopdracht/tests/Unit/TeacherTest.php
+++ PHP_Eindopdracht/tests/Unit/TeacherTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\Course;
+use App\Teacher;
+use App\User;
 use PHPUnit\Framework\TestCase;
 
 class TeacherTest extends TestCase
@@ -11,8 +14,55 @@ class TeacherTest extends TestCase
      *
      * @return void
      */
+/*    private $user;
+
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->user = User::first();
+
+    }*/
+
     public function testExample()
     {
         $this->assertTrue(true);
+    }
+
+/*    public function testWhen_TeacherGiven_Expect_TeacherWithEmail()
+    {
+        $teacher = new Teacher();
+
+        $teacher->setEmailAttribute('jasper@example.com');
+
+
+        $this->assertEquals('jasper@example.com', $teacher->getEmailAttribute('jasper@example.com'));
+    }*/
+
+    public function testWhen_CourseGiven_Expect_Course()
+    {
+        $teacher = new Teacher();
+        $teacher->id = 1;
+        $teacher->name = 'Jasper';
+
+        $course = new Course();
+        $course->id = 1;
+        $course->name = 'webphp';
+        $course->period = 1;
+        $course->year = 1;
+        $course->coordinator = $teacher->id;
+        $course->teacher = $teacher->id;
+        $course->exam_method_id = 1;
+        $course->study->points = 2;
+
+        $this->assertEquals('2', $course->getTotalBlockPoints($course->year, $course->period));
+    }
+
+    public function testWhen_UserGiven_ExpectUserAdmin()
+    {
+/*        $user = User::orderBy('id', 'desc')->firstOrFail()->get();*/
+
+        $this->assertEquals('Admin', $this->user->roles->name);
     }
 }
