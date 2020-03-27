2ece1094571564af2e55827a87581c656184ec2f
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-27 13:21:40 +0100

    multiple dropdown edit

--- PHP_Eindopdracht/app/Http/Controllers/CourseController.php
+++ PHP_Eindopdracht/app/Http/Controllers/CourseController.php
@@ -51,18 +51,29 @@ class CourseController extends Controller
         $course = Course::findOrFail($id);
         $teachers = Teacher::orderBy('id', 'desc')->get();
         $exam_methods = ExamMethod::all();
-        return view('Course/edit', compact('course', 'teachers', 'exam_methods'));
+        $teachers2 = [];
+        $count = 0;
+        foreach ($course->teachers as $teacher){
+            $teachers2[$teacher->id] = $teacher;
+            $count++;
+        }
+        return view('Course/edit', compact('course', 'teachers', 'exam_methods' , 'teachers2'));
     }
 
-    public function update(Request $request, $id){
-
-        Course::where('id', $id)->firstOrFail()->update([
+    public function update(Request $request, $id)
+    {
+        $course = Course::where('id', $id)->firstOrFail();
+        $course->update([
             'name' => $request->name,
             'period' => $request->period,
             'coordinator' => $request->input('coordinator'),
             'exam_method_id' => $request->input('test_method'),
             'study_points' => $request->study_points,
         ]);
+
+        $course->teachers()->detach($course->teachers);
+        $course->teachers()->attach(request('teachers_course'));
+        $course->save();
         return redirect('/admin');
     }
 
